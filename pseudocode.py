379de99e61871baa200aa65618fa0daafb25cfd0
WofWca <wofwca@protonmail.com>
2022-05-21 14:54:12 +0300

    rename a var

--- pseudocode.py
+++ pseudocode.py
@@ -1,7 +1,7 @@
 import os
 
 def on_new_email(email_file_path):
-    recipiend_email = get_recipiend_email()
+    get_recipiend_email_address = get_recipiend_email_address()
     # TODO protect from code injections.
-    os.popen(f'gpg -r {recipiend_email} {email_file_path}')
+    os.popen(f'gpg -r {get_recipiend_email_address} {email_file_path}')
     os.popen(f'ipfs add {email_file_path}.gpg')
