b7ef86dd5a8a59a75e253b1c9ccb2f8dd9b5dd72
WofWca <wofwca@protonmail.com>
2022-05-21 17:39:12 +0300

    more pseudocode

--- pseudocode.py
+++ pseudocode.py
@@ -3,5 +3,9 @@ import os
 def on_new_email(email_file_path):
     get_recipiend_email_address = get_recipiend_email_address()
     # TODO protect from code injections.
+    # TODO make sure that the file name is not public.
+    # TODO maybe it's possible to retrieve the recipient's public key from the email itself? Or at least
+    # its fingerprint or something so we can download the key from key databases to verify it?
     os.popen(f'gpg -r {get_recipiend_email_address} {email_file_path}')
-    os.popen(f'ipfs add {email_file_path}.gpg')
+    cid = os.popen(f'ipfs add -Q {email_file_path}.gpg')
+    # TODO somehow make the user able to receive CID do download the email.
