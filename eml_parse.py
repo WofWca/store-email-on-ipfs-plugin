73309efdc7c8f7efc39b600ded54ba528baa8c13
Valery Senotov <senotovel@gmail.com>
2022-05-22 14:13:45 +0300

    upd: eml parsing functionality

--- eml_parse.py
+++ eml_parse.py
@@ -21,7 +21,6 @@ def get_recipiend_email_address(email_file_path: str = "./tests/sample.eml") -> 
 
     ep = eml_parser.EmlParser()
     parsed_eml = ep.decode_email(email_file_path)
-    print(parsed_eml)
 
     recipiend_email_addresses = parsed_eml['header']['to']
     return recipiend_email_addresses
@@ -30,11 +29,10 @@ def get_contents(email_file_path: str = "./tests/sample.eml") -> dict:
 
     ep = eml_parser.EmlParser(include_raw_body=True)
     parsed_eml = ep.decode_email(email_file_path)
-    print(parsed_eml)
 
     contents = parsed_eml['body']
     return contents
 
-if __name__ == '__main__':
-    print(get_recipiend_email_address())
-    print(get_contents())
+# if __name__ == '__main__':
+#     print(get_recipiend_email_address())
+#     print(get_contents())
